e9b66bec818f48d58374e3075c678903d4efff8b
hany049 <kdw6034@gmail.com>
2025-10-20 22:54:50 +0900

    feat: 커스텀 구분자로 숫자 분리

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -6,6 +6,19 @@ public class Calculator {
 
     public int add(String inputString) {
 
+        if (inputString.startsWith("//")) {
+            int customIndex = inputString.indexOf("\\n");
+            if (customIndex != -1) {
+                String CustomSeparator = inputString.substring(2, customIndex);
+                String remainString = inputString.substring(customIndex + 2);
+
+                String CustomRegex = "[,:" + CustomSeparator + "]";
+
+                String[] number = remainString.split(CustomRegex);
+                return Arrays.stream(number).mapToInt(Integer::parseInt).sum();
+            }
+        }
+
         String[] number = inputString.split("[,:]");
 
         return Arrays.stream(number).mapToInt(Integer::parseInt).sum();
