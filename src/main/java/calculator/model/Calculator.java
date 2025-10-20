dad53658b45c2f56b94ae636b7e4ab0b4e893396
hany049 <kdw6034@gmail.com>
2025-10-20 23:40:09 +0900

    feat: 입력값 검증 및 예외처리(IllegalArgumentException)

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -5,22 +5,56 @@ import java.util.Arrays;
 public class Calculator {
 
     public int add(String inputString) {
+        checkBlank(inputString);
 
         if (inputString.startsWith("//")) {
             int customIndex = inputString.indexOf("\\n");
             if (customIndex != -1) {
                 String CustomSeparator = inputString.substring(2, customIndex);
+                verifyCustomSeparator(CustomSeparator);
                 String remainString = inputString.substring(customIndex + 2);
 
                 String CustomRegex = "[,:" + CustomSeparator + "]";
 
                 String[] number = remainString.split(CustomRegex);
-                return Arrays.stream(number).mapToInt(Integer::parseInt).sum();
+                return Arrays.stream(number)
+                        .mapToInt(this::checkNumber)
+                        .peek(this::validateNumber)
+                        .sum();
             }
         }
 
         String[] number = inputString.split("[,:]");
 
-        return Arrays.stream(number).mapToInt(Integer::parseInt).sum();
+        return Arrays.stream(number)
+                .mapToInt(this::checkNumber)
+                .peek(this::validateNumber)
+                .sum();
+    }
+
+    private int checkNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요!!!");
+        }
+    }
+
+    private void validateNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("양수만 입력해주세요!!!");
+        }
+    }
+
+    private void checkBlank(String string) {
+        if (string.isBlank()) {
+            throw new IllegalArgumentException("문자열을 입력해 주세요!!!");
+        }
+    }
+
+    private void verifyCustomSeparator(String custom) {
+        if (custom.matches(".*\\d.*")) {
+            throw  new IllegalArgumentException("커스텀 구분자로 숫자를 사용할 수 없어요!!!");
+        }
     }
 }
